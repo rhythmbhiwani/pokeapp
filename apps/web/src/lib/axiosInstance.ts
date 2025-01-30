852710b7800f6d5559f84e79ba28bee63a08ba6f
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2025-01-31 01:51:47 +0530

    Added logs

--- apps/web/src/lib/axiosInstance.ts
+++ apps/web/src/lib/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+console.log("API_BASE_URL", API_BASE_URL);
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
