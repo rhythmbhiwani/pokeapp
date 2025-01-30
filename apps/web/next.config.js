09ad1a030b2374b4c587d64d34dad2ff37d43345
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2025-01-31 00:24:50 +0530

    Configuration Added

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,16 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, "../../"),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
